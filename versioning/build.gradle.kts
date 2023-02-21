6dc34b71d53516e570cbc25de44b3f7af7807569
David Greven <opensource@grevend.dev>
2023-02-21 17:51:28 +0100

    Create VersionScheme SPI for core-libs/versioning
    Signed-off-by: David Greven <opensource@grevend.dev>

---
+++ versioning/build.gradle.kts
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/whichlicense/core-libs.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("signing")
+}
+
+group = "com.whichlicense"
+version = "0.0.0"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("versioning") {
+            artifactId = "versioning"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("WhichLicense core-libs/versioning")
+                description.set("This library provides the VersionScheme SPI interface to facilitate verifying and parsing version information.")
+                url.set("https://github.com/whichlicense/core-libs/versioning")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("grevend")
+                        name.set("David Greven")
+                        email.set("david.greven@whichlicense.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/whichlicense/core-libs.git")
+                    developerConnection.set("scm:git:git@github.com:whichlicense/core-libs.git")
+                    url.set("https://github.com/whichlicense/core-libs")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/whichlicense/core-libs")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["versioning"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
---
+++ versioning/src/main/java/com/whichlicense/versioning/VersionScheme.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/whichlicense/core-libs.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.whichlicense.versioning;
+
+/**
+ * The VersionScheme SPI interface to facilitate verifying and parsing version information.
+ *
+ * @author David Greven
+ * @version 0
+ * @since 0.0.0
+ */
+public interface VersionScheme {
+}
