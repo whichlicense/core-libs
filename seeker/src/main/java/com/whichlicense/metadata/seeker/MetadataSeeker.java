ace7c6b48b95c8e8f718c4b5bc05c9c1a0c0b506
David Greven <opensource@grevend.dev>
2023-04-06 15:49:45 +0200

    Add core-libs/seeker documentation
    Signed-off-by: David Greven <opensource@grevend.dev>

--- seeker/src/main/java/com/whichlicense/metadata/seeker/MetadataSeeker.java
+++ seeker/src/main/java/com/whichlicense/metadata/seeker/MetadataSeeker.java
@@ -8,7 +8,27 @@ package com.whichlicense.metadata.seeker;
 
 import java.util.Set;
 
+/**
+ * An SPI interface that represents a metadata seeker.
+ *
+ * @author David Greven
+ * @version 0
+ * @since 0.0.0
+ */
 public interface MetadataSeeker {
+    /**
+     * Returns a set of glob patterns that specify the metadata to be sought.
+     *
+     * @return A set of glob patterns as strings.
+     * @since 0.0.0
+     */
     Set<String> globs();
+
+    /**
+     * Returns the type of metadata source that the seeker is designed to work with.
+     *
+     * @return The type of metadata source as a MetadataSourceType enum value.
+     * @since 0.0.0
+     */
     MetadataSourceType type();
 }
